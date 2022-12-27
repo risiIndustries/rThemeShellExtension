375190985ffca351240fe0c1045469c7b38853eb
PizzaLovingNerd <pizzalovingnerd@teknik.io>
2022-12-26 20:12:31 -0800

    Update extension.js

--- extension.js
+++ extension.js
@@ -2,27 +2,32 @@ const { Gio, GLib } = imports.gi;
 const Main = imports.ui.main;
 const Mainloop = imports.mainloop;
 
-let old_stylesheet;
-let changed;
-let rtheme = Gio.Settings.new("io.risi.rtheme");
-
 function init () {};
 
 function enable () {
-    changed = rtheme.connect("changed", (setting, key) => {
-        Mainloop.timeout_add(1000, () => apply_theme())
+    let old_stylesheet = Main.getThemeStylesheet();
+    let rtheme = Gio.Settings.new("io.risi.rtheme");
+    let theme_timeout;
+    
+    let changed = rtheme.connect("changed", (setting, key) => {
+        let theme_timeout = Mainloop.timeout_add(1000, () => apply_theme())
     })
-    apply_theme()
+    apply_theme();
 };
 
 function disable () {
     Main.setThemeStylesheet(old_stylesheet)
     Main.loadTheme();
     rtheme.disconnect(changed);
+    
+    if (theme_timeout) {
+        GLib.Source.remove(theme_timeout);
+        theme_timeout = null;
+    }
+    old_stylesheet = null;
 };
 
 function apply_theme () {
-    old_stylesheet = Main.getThemeStylesheet();
     let stylesheet = GLib.build_filenamev(
         [GLib.get_user_config_dir(), 'rtheme', 'shell', 'rtheme.css']
     );
